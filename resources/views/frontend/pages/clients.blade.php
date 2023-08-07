cb51ce2cca86150b01dbc102ffe319bc18ba4932
anmolko <anmol@canosoft.com.np>
2023-08-07 21:36:40 +0545

    clients update

--- resources/views/frontend/pages/clients.blade.php
+++ resources/views/frontend/pages/clients.blade.php
@@ -2,55 +2,105 @@
 @section('title') Clients @endsection
 @section('css')
     <style>
-        .img-wrapper {
-            height: 100px;
-            object-fit: cover;
+        .tile {
+            display: none;
         }
-        #gallery img.img-responsive {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
+        .btn-filter {
+            position: relative;
+            display: inline-block;
+            font-size: 14px;
+            text-transform: uppercase;
+            color: #1e2434;
+            text-align: center;
+            padding: 14px 30px;
+            background-color: #fff;
+            border: 1px solid #e1e8e4;
+            cursor: pointer;
+            margin: 0px 8.5px;
+            margin-bottom: 15px;
+            -webkit-transition: all 500ms ease;
+            transition: all 500ms ease;
+        }
+
+        .btn-filter.active{
+            color: var(--text-color-bg-theme-color1);
+            background-color: var(--theme-color1);
+        }
+        .scale-anm img{
+            -webkit-transition: all 300ms ease;
+            transition: all 300ms ease;
+            -webkit-box-shadow: 0 10px 60px rgba(0, 0, 0, 0.07);
+            box-shadow: 0 10px 60px rgba(0, 0, 0, 0.07);
+            padding: 15px;
         }
     </style>
-    <link rel="stylesheet" href="{{asset('assets/frontend/css/plugins/lightbox.css')}}">
+{{--    <link rel="stylesheet" href="{{asset('assets/frontend/css/plugins/lightbox.css')}}">--}}
 @endsection
 @section('content')
 
-    <!-- Breadcrumbs Start -->
-    <div class="rs-breadcrumbs img6">
-        <div class="container">
-            <div class="breadcrumbs-inner">
-                <h1 class="page-title">Our Clients</h1>
+
+    <section class="page-title" style="background-image: url({{ asset('assets/frontend/images/background/page-title.jpg') }});">
+        <div class="auto-container">
+            <div class="title-outer">
+                <h1 class="title">Our Clients</h1>
+                <ul class="page-breadcrumb">
+                    <li><a href="/">Home</a></li>
+                    <li>Our Clients</li>
+                </ul>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div class="rs-project style2 pt-100 pb-100 md-pt-70 md-pb-70">
+    <!--Start courses Details-->
+    <section class="course-details">
         <div class="container">
-            <div class="gridFilter mb-50 md-mb-30 text-center">
-                <button class="active" data-filter="*">All</button>
-                @foreach($country as $index=>$cn)
-                    <button data-filter=".{{$index}}">{{ ucfirst($cn) }}</button>
-                @endforeach
-
-            </div>
-            <div class="row grid rs-project">
-
-                @foreach($clients as $client)
-                    <div class="col-lg-4 col-md-6 mb-30 grid-item {{$client->country}}">
-                        <div class="box-inner">
-                            <div class="box-item">
-                                <div class="icon-box">
-                                    <a href="#"><img class="dance_hover" src="{{asset('/images/clients/'.@$client->image)}}" alt=""></a>
+            <div class="row flex-xl-row-reverse">
+                <!--Start courses Details Content-->
+                <div class="col-xl-12 col-lg-12">
+                    <div class="courses-details__content">
+                        <section class="product-description">
+                            <div class="container pt-0 pb-90">
+                                <div class="product-discription">
+                                    <div class="tabs-box">
+                                        <div id="filters" class="toolbar mb2 mt2">
+                                            <button class="btn-filter fil-cat filter active"  data-filter="all">All</button>
+                                            @foreach($country as $index=>$cn)
+                                                <button class="btn-filter fil-cat filter" data-rel="{{$index}}" data-filter=".{{$index}}">{{ ucfirst($cn) }}</button>
+                                            @endforeach
+                                        </div>
+                                        <div id="portfolio"  class="row">
+                                            @foreach($clients as $client)
+                                                <div class="mt-4 col-lg-3 col-md-3 tile scale-anm {{$client->country}}">
+                                                        <a href="#"><img src="{{asset('/images/clients/'.@$client->image)}}" alt=""></a>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </section>
                     </div>
-                @endforeach
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
 @section('js')
-    <script src="{{asset('assets/frontend/js/plugins/lightbox.min.js')}}"></script>
+    <script src="http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js"></script>
+    <script>
+        $('#portfolio').mixItUp({
+
+            selectors: {
+                target: '.tile',
+                filter: '.filter',
+                sort: '.sort-btn'
+            },
+
+            animation: {
+                animateResizeContainer: false,
+                effects: 'fade scale'
+            }
+
+        });
+    </script>
 @endsection
